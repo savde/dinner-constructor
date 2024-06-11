82b04bea7d2eec5d86bf0e2379c3f02e934d5dda
savde <savickiydmitriy94@gmail.com>
2024-06-11 23:07:08 +0700

    Исправление ошибок

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -20,14 +20,14 @@ public class DinnerConstructor {
 
     void generateDishes(int count, ArrayList<String> types) { //генерация комбинаций блюд
         for (int i = 0; i < count; i++) {
-            System.out.println("Комбо " + i);
+            System.out.println("Комбо " + (i+1));
             ArrayList<String> combo = new ArrayList<>();
             for (String type : types) {
                 if (dinnerMenu.containsKey(type)) {
                     ArrayList<String> dishes = dinnerMenu.get(type);
                     String dishName = dishes.get(rand.nextInt(dishes.size() - 1));
                     combo.add(dishName);
-                } else System.out.println("Типа " + type + " не существует.");
+                }
             }
             System.out.println(combo);
         }
